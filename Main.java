cfce60b160194ffb42410d836d88fdfaefe26340
lukemassa-hiro <d4rkoladelas@gmail.com>
2025-11-13 23:14:17 -0300

    Recursividade implementada

--- Main.java
+++ Main.java
@@ -1,31 +1,36 @@
-
-
 public class Main {
     public static void main(String[] args) {
         int[] vetor = new int[7];
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = (int) (Math.random() * vetor.length);
-        }
+
+        preencher(vetor, 0);
+
         System.out.println("Os valores do vetor são: ");
-        for (int i = 0; i < vetor.length; i++) {
+        imprimir(vetor, 0);
 
-            System.out.println(vetor[i]);
-        }
         System.out.println("O resultado das somas dos vetores é: ");
-
-        System.out.println(somar( 0,0,vetor));
+        System.out.println(somar(0, 0, vetor));
     }
-        private static int somar(int soma, int posicao, int[] vetor){
-            if (posicao < vetor.length){
-                soma = soma + vetor[posicao];
-                return somar(soma, posicao + 1, vetor);
 
-            }else {
-                return soma;
-
-            }
-
+    private static void preencher(int[] vetor, int pos) {
+        if (pos < vetor.length) {
+            vetor[pos] = (int) (Math.random() * vetor.length);
+            preencher(vetor, pos + 1);
         }
+    }
 
+    private static void imprimir(int[] vetor, int pos) {
+        if (pos < vetor.length) {
+            System.out.println(vetor[pos]);
+            imprimir(vetor, pos + 1);
         }
+    }
 
+    private static int somar(int soma, int posicao, int[] vetor) {
+        if (posicao < vetor.length) {
+            soma = soma + vetor[posicao];
+            return somar(soma, posicao + 1, vetor);
+        } else {
+            return soma;
+        }
+    }
+}
